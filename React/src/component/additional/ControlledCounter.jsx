ac8dba23ebe2b34e2aeb593714587a2d735303c8
AbimannanAnbalagan <127296973+AbimannanAnbalagan@users.noreply.github.com>
2023-07-25 12:45:44 +0530

    day1 source file

---
+++ React/src/component/additional/ControlledCounter.jsx
@@ -0,0 +1,11 @@
+import React,{useState} from "react";
+
+const ControlledCounter = () =>
+{
+    <div>
+        <label >Enter A Value:</label>
+        <input type="text"></input>
+    </div>
+};
+
+export default ControlledCounter;

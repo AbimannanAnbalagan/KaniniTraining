8af3b896182a2f14584fed5b7fa701ffcab41909
AbimannanAnbalagan <127296973+AbimannanAnbalagan@users.noreply.github.com>
2023-07-25 12:47:44 +0530

    MyReact source file

---
+++ React/src/Classprop.js
@@ -0,0 +1,14 @@
+// import React,{Component} from "react";
+
+// class Classprop extends Component{
+//     render(){
+//         return(
+//             <div>
+//                 <h2>Hii</h2>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Classprop;
+
--- React/src/index.js
+++ React/src/index.js
@@ -5,17 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// const head1 = 
-// (
-// <div>
-// <h1>React Example h1</h1> <h2>React Example h2</h2> 
-// </div>
-// );
-
-root.render
-(
-  // head1
-  <App/>
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
